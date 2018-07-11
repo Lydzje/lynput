79d65956f9343912b2e241b2182a50ae0b2e248d
Lydzje <to.lydzje@gmail.com>
2018-07-12 00:36:03 +0200

    Removed files I shouldn't have merged

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-printf "\n[CLEANING...]\n\n"
-rm lynput.love -v
-printf "\n[CLEANING COMPLETED]\n\n"
-
-printf "\n[BUILDING LOVE PROJECT...]\n\n"
-zip -r ./lynput.love ./ -x .*/\* build/\* .* *.sh *.md  *.love
-printf "\n[BUILDING COMPLETED]\n\n"
