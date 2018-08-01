b4da6fc14241f5b41a1f6142a1be644afe6ea0fa
Lydzje <to.lydzje@gmail.com>
2018-08-01 16:58:16 +0200

    Big update
    - More efficient use of tables
    - Now inputsSet uses Lynput terminology for inputs instead of LÖVE terminology
    - Unbinding works as expected now
    - Source code is cleaner now

--- Lynput.lua
+++ Lynput.lua
@@ -7,31 +7,40 @@ Lynput.s_lynputs = {}
 Lynput.s_idCount = 0
 Lynput.s_count = 0
 
+-- TODO: More reserved words such as:
+-- - reserved characters
+-- - lynput variables stored in "self"
 Lynput.s_reserved_words = {
   "and", "break", "do", "else", "elseif", "end", "false", "for", 
   "function", "if", "in", "local", "nil", "not", "or", "repeat", 
   "return", "then", "true", "until", "while"
 }
 
-Lynput.s_mouse_buttons = {
-  lmb="1", rmb="2", mmb="3", mb4="x1", mb5="x2"
+----------------
+-- DICTIONARIES
+----------------
+Lynput.s_mouseButtons = {
+  ["1"]="lmb", ["2"]="rmb", ["3"]="mmb", ["x1"]="mb4", ["x2"]="mb5"
 }
 
-Lynput.s_mouse_axes = {
-  "wd", "wu"
-}
+-----------------------
+-- FUTURE DICTIONARIES
+-----------------------
+-- Lynput.s_mouse_axes = {
+--   "wd", "wu"
+-- }
 
-Lynput.s_gamepad_axes = {
-  "leftx", "lefty", "rightx", "righty", "triggerleft", "triggerright"
-}
+-- Lynput.s_gamepad_axes = {
+--   "leftx", "lefty", "rightx", "righty", "triggerleft", "triggerright"
+-- }
 
--- FIXME: Input names have to be unique, conflict with keyboard
--- May add a preffix like gstart for start or ga for a, or gamepad_a for a
-Lynput.s_gamepad_buttons = {
-  "a", "b", "x", "y", "back", "guide", "start", "leftstick", 
-  "rightstick", "leftshoulder", "rightshoulder", "dppup", "dpdown", 
-  "dpleft", "dpright"
-}
+-- -- FIXME: Input names have to be unique, conflict with keyboard
+-- -- May add a preffix like gstart for start or ga for a, or gamepad_a for a
+-- Lynput.s_gamepad_buttons = {
+--   "a", "b", "x", "y", "back", "guide", "start", "leftstick", 
+--   "rightstick", "leftshoulder", "rightshoulder", "dppup", "dpdown", 
+--   "dpleft", "dpright"
+-- }
 
 
 function Lynput:new()
@@ -44,25 +53,13 @@ function Lynput:new()
 end
 
 
-local function isActionValid(action)
+local function _isActionValid(action)
   if type(action) ~= "string" then
-    -- TODO: "Could not bind INPUT TO ACTION"
-    error(
-      "Could not bind action->" .. action .. 
-      " to input, the action is not a string"
-    )
     return false
   end -- if not string
   
-  -- TODO: Not valid if contains spaces or reserved characters
-  
   for i,v in ipairs(Lynput.s_reserved_words) do
     if v == action then
-      -- TODO: "Could not bind INPUT TO ACTION"
-      error(
-        "Could not bind action->" .. action .. 
-        " to input, the action name is a reserved word"
-      )
       return false
     end -- if s_reserved_word
   end -- for each s_reserved_words
@@ -71,17 +68,18 @@ local function isActionValid(action)
 end
 
 
-local function isInputValid(input)
-  if Lynput.s_mouse_buttons[input] then
-    return true, "mButton"
-  elseif love.keyboard.getScancodeFromKey(input) then
-    return true, "keyboard"
-  end -- if input exists
+local function _isInputValid(input)
+  for k,v in pairs(Lynput.s_mouseButtons) do
+    if v == input then
+      return true
+    end -- if v == input
+  end -- for each LÖVE mouse button
   
-  -- TODO: Gamepad axes
   -- TODO: Gamepad buttons
+  -- TODO: Gamepad axes
   -- TODO: Touch screen
-  return false
+
+  return love.keyboard.getScancodeFromKey(input)
 end
 
 
@@ -91,116 +89,119 @@ function Lynput:remove()
 end
 
 
-function Lynput:bind(action, input)
-  -- TODO: input has to be an array of inputs
-  local actionValid = isActionValid(action)
-  local inputValid, type = isInputValid(input)
-  if actionValid and inputValid then
-    local input = input
-    if type == "mButton" then
-      input = Lynput.s_mouse_buttons[input]
-    end -- if mButton
+function Lynput:bind(action, inputs)
+  -- Transforms 1 input to a table
+  if type(inputs) == "string" then
+    local input = inputs
+    inputs = {}
+    inputs[1] = input
+  end -- if only one input was given
+
+  -- Process input
+  for _,input in ipairs(inputs) do
+    -- Is action valid?
+    assert(
+      _isActionValid(action),
+      "Could not bind input->" .. input ..
+      -- TODO: Check documentation for valid actions message
+	"  to action->" .. action .. ", the action is not valid"
+    )
+    -- Is input valid?
+    assert(
+      _isInputValid(input),
+      "Could not bind input->" .. input ..
+      -- TODO: Check documentation for valid inputs message
+	"  to action->" .. action .. ", the input is not valid"
+    )
     
     -- FIXME: Pressed and released do not work for movement inputs
-      if not self[action] then
-        -- action not set yet
-        self[action] = {}
-        self[action].pressed = false
-        self[action].released = false
-        self[action].holding = false
-        self[action].inputs = {}
-
-      if self.inputsSet[input] then
-        -- input in use
-        actionSet = self.inputsSet[input]
-        i = 1
-        found = false
-        len = #(self[actionSet].inputs)
-        while i <= len and not found do
-          if self[actionSet].inputs[i] == input then
-            found = true
-            -- removed input in the action it was assigned to
-            self[actionSet].inputs[i] = nil
-          end -- if input found
-        end -- while input not found
-      end -- if input already in use
+    if not self[action] then
+      self[action] = {}
+      self[action].pressed = false
+      self[action].released = false
+      self[action].holding = false
     end -- if action not set
     
     self.inputsSet[input] = action
-    len = #(self[action].inputs)
-    self[action].inputs[len+1] = input
-  end -- if action and input are valid
+  end -- for each input
 end
 
 
-function Lynput:unbind(action, input)
-  -- TODO: input has to be an array of inputs
-  if self[action] then
-    if self.inputsSet[input] then
-      self.inputsSet[input] = nil
-      for i,v in ipairs(self[action].inputs) do
-        if v == input then
-          self[action].inputs[i] = nil
-          break
-        end -- if input found
-      end -- for each input assigned to action
-    else
-      error(
+function Lynput:unbind(action, inputs)
+  -- Transforms 1 input to a table
+  if type(inputs) == "string" then
+    local input = inputs
+    inputs = {}
+    inputs[1] = input
+  end -- if only one input was given
+
+  -- Process input
+  for _,input in ipairs(inputs) do
+    -- Is action set?
+    assert(
+      self[action],
       "Could not unbind input->" .. input .. 
-      " to action->" .. action .. ", the input is not set"
-      )
-    end -- if input is set
-  else
-    error(
-      "Could not unbind input->" .. input .. 
-      " to action->" .. action .. ", the action is not set"
+	"  to action->" .. action .. ", the action is not set"
     )
-  end -- if action is set
+    -- Is input set?
+    assert(
+      self.inputsSet[input],
+      "Could not unbind input->" .. input .. 
+	"  to action->" .. action .. ", the input is not set"
+    )
+    
+    self.inputsSet[input] = nil
+    self[action].pressed = false
+    self[action].holding = false
+    self[action].released = false
+  end -- for each input
 end
 
 
 function Lynput:unbindAll(action)
-  if self[action] then
-    local len = #self[action].inputs
-    for i=1,len do
-      local input = self[action].inputs[i]
-      self.inputsSet[input] = nil
-      self[action].inputs[i] = nil
-    end -- for each input assigned to action
-  else
-    error(
-      "Could not unbind all inputs in action->" .. action 
-      .. ", the action is not set"
-    )
-  end -- if action is set
+  -- Is action set?
+  assert(
+    self[action],
+    "Could not unbind all inputs in action->" .. action ..
+      ", the action is not set"
+  )
+
+  for k, v in pairs(self.inputsSet) do
+    if v == action then
+      self.inputsSet[k] = nil
+    end -- if v == action
+  end -- for each input set
 end
 
 
 function Lynput:removeAction(action)
-  if self[action] then
-    self:unbindAll(action)
-    self[action] = nil
-  else
-    error(
-      "Could not remove action->" .. action .. ", the action is not set"
-    )
-  end -- if action is set
+  -- Is action set?
+  assert(
+    self[action],
+    "Could not remove action->" .. action ..
+      ", the action is not being used"
+  )
+  
+  self:unbindAll(action)
+  self[action] = nil
 end
 
 
 function Lynput:update()
-  for k,v in pairs(self.inputsSet) do
+  -- It's not possible to iterate actions through "self" because it
+  -- also contains the inputsSet table
+  for _,v in pairs(self.inputsSet) do
     -- FIXME: Only works if lynput is updated after input processing
     self[v].pressed = false
     self[v].released = false
-  end
+  end -- for each action set
 end
 
 
------------------------------
+---------------------------------
 -- KEYBOARD CALLBACKS
------------------------------
-function Lynput.key_pressed(key)
+---------------------------------
+function Lynput.onkeypressed(key)
   for k,v in pairs(Lynput.s_lynputs) do
     if v.inputsSet[key] then
       action = v.inputsSet[key]
@@ -212,7 +213,7 @@ function Lynput.key_pressed(key)
 end
 
 
-function Lynput.key_released(key)
+function Lynput.onkeyreleased(key)
   for k,v in pairs(Lynput.s_lynputs) do
     if v.inputsSet[key] then
       action = v.inputsSet[key]
@@ -224,11 +225,13 @@ function Lynput.key_released(key)
 end
 
 
------------------------------
+--------------------------------------
 -- MOUSE CALLBACKS
------------------------------
-function Lynput.mouse_pressed(button)
-  button = tostring(button)
+--------------------------------------
+function Lynput.onmousepressed(button)
+  -- Translate LÖVE button to Lynput button
+  button = Lynput.s_mouseButtons[tostring(button)]
+  -- Process button
   for k,v in pairs(Lynput.s_lynputs) do
     if v.inputsSet[button] then
       action = v.inputsSet[button]
@@ -240,8 +243,10 @@ function Lynput.mouse_pressed(button)
 end
 
 
-function Lynput.mouse_released(button)
-  button = tostring(button)
+function Lynput.onmousereleased(button)
+  -- Translate LÖVE button to Lynput button
+  button = Lynput.s_mouseButtons[tostring(button)]
+  -- Process Lynput button
   for k,v in pairs(Lynput.s_lynputs) do
     if v.inputsSet[button] then
       action = v.inputsSet[button]
--- main.lua
+++ main.lua
@@ -3,15 +3,9 @@ function love.load()
   lynput = Lynput()
   lynput:bind("exit", "escape")
   
-  -- keyboard
-  lynput:bind("pressing", "p")
-  lynput:bind("releasing", "r")
-  lynput:bind("holding", "h")
-  
-  -- mouse buttons
-  lynput:bind("pressing", "lmb")
-  lynput:bind("releasing", "rmb")
-  lynput:bind("holding", "mmb")
+  lynput:bind("pressing", {"p", "lmb"})
+  lynput:bind("releasing", {"r", "rmb"})
+  lynput:bind("holding", {"h", "mmb"})
   
   lynput:unbindAll("holding")
 end
@@ -21,12 +15,16 @@ function love.update(dt)
   if lynput.exit.released then
     love.event.quit()
   end -- if exit
+
   if lynput.pressing.pressed then
     print("Pressing")
   end -- if pressing
+
   if lynput.releasing.released then
     print("Releasing")
+    lynput:unbind("releasing", {"r", "rmb"})
   end -- if releasing
+
   if lynput.holding.holding then
     print("Holding")
   end -- if holding
@@ -44,20 +42,20 @@ end
 -- CALLBACKS
 -----------------------------
 function love.keypressed(key)
-  Lynput.key_pressed(key)
+  Lynput.onkeypressed(key)
 end
 
 
 function love.keyreleased(key)
-  Lynput.key_released(key)
+  Lynput.onkeyreleased(key)
 end
 
 
 function love.mousepressed(x, y, button, istouch)
-  Lynput.mouse_pressed(button)
+  Lynput.onmousepressed(button)
 end
 
 
 function love.mousereleased(x, y, button, istouch)
-  Lynput.mouse_released(button)
+  Lynput.onmousereleased(button)
 end
